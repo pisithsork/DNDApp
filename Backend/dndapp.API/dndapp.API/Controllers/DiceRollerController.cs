e29f5fd2667c18f596d172cc8de338bf3bc0d500
pisithsork <55104592+pisithsork@users.noreply.github.com>
2023-01-27 12:07:51 -0500

    Forgot to get rid of old code

--- Backend/dndapp.API/dndapp.API/Controllers/DiceRollerController.cs
+++ Backend/dndapp.API/dndapp.API/Controllers/DiceRollerController.cs
@@ -20,47 +20,5 @@ namespace dndapp.API.Controllers
         {
             return diceroller.rolldice(numofdice, dicetype);
         }
-
-/*        [HttpGet("{numofdice}d4")]
-        public int[] getd4(int numofdice)
-        {
-            return diceroller.rolld4(numofdice);
-        }
-
-        [HttpGet("{numofdice}d6")]
-        public int[] getd6(int numofdice)
-        {
-            return diceroller.rolld6(numofdice);
-        }
-
-        [HttpGet("{numofdice}d8")]
-        public int[] getd8(int numofdice)
-        {
-            return diceroller.rolld8(numofdice);
-        }
-
-        [HttpGet("{numofdice}d10")]
-        public int[] getd10(int numofdice)
-        {
-            return diceroller.rolld10(numofdice);
-        }
-
-        [HttpGet("{numofdice}d12")]
-        public int[] getd12(int numofdice)
-        {
-            return diceroller.rolld12(numofdice);
-        }
-
-        [HttpGet("{numofdice}d20")]
-        public int[] getd20(int numofdice)
-        {
-            return diceroller.rolld20(numofdice);
-        }
-
-        [HttpGet("{numofdice}d100")]
-        public int[] getd100(int numofdice)
-        {
-            return diceroller.rolld100(numofdice);
-        }*/
     }
 }
